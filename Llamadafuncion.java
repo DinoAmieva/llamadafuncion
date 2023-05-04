ba6af3b83a2abda77e5f4c5c331a92a872dc09a0
Dino Amieva <125599893+DinoAmieva@users.noreply.github.com>
2023-05-04 11:37:08 -0600

    Add files via upload

---
+++ Llamadafuncion.java
@@ -0,0 +1,15 @@
+
+package llamadafuncion;
+
+
+public class Llamadafuncion {
+
+int metodoEntero()//Función sin parámetros
+{
+    int suma = 5+5;
+    return suma; //Acá termina la ejecución del método
+    //return 5+5;//Este return nunca se ejecutará
+    //Intenta intercambiar la línea 3 con la 5
+    //int x = 10; //Esta línea nunca se ejecutará
+    }
+}
